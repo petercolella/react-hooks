5a5d742a8497f41ec01a22bdf034bcc9e0a398fb
petercolella <peter.joseph.colella@gmail.com>
2018-12-27 09:10:41 -0500

    Section 26 Lecture 423

---
+++ src/components/Todo.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const todo = props => {
+  const inputState = useState('');
+
+  const inputChangeHandler = event => {
+    inputState[1](event.target.value);
+  };
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={inputState[0]}
+      />
+      <button type="button">Add</button>
+      <ul />
+    </React.Fragment>
+  );
+};
+
+export default todo;
