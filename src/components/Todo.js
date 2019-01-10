e1b42292770367b6d49a23a0772f78cf43217538
petercolella <peter.joseph.colella@gmail.com>
2019-01-09 23:49:30 -0500

    Section 26, Lecture 434

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 
 const todo = props => {
   const [todoName, setTodoName] = useState('');
+  const [submittedTodo, setSubmittedTodo] = useState(null);
   const [todoList, setTodoList] = useState([]);
   //   const [todoState, setTodoState] = useState({ userInput: '', todoList: [] });
 
@@ -34,6 +35,15 @@ const todo = props => {
     };
   }, []);
 
+  useEffect(
+    () => {
+      if (submittedTodo) {
+        setTodoList(todoList.concat(submittedTodo));
+      }
+    },
+    [submittedTodo]
+  );
+
   const inputChangeHandler = event => {
     // setTodoState({
     //   userInput: event.target.value,
@@ -47,13 +57,16 @@ const todo = props => {
     //   userInput: todoState.userInput,
     //   todoList: todoState.todoList.concat(todoState.userInput)
     // });
-    setTodoList(todoList.concat(todoName));
+
     axios
       .post('https://react-hooks-ff4e9.firebaseio.com/todos.json', {
         name: todoName
       })
       .then(res => {
-        console.log(res);
+        setTimeout(() => {
+          const todoItem = { id: res.data.name, name: todoName };
+          setSubmittedTodo(todoItem);
+        }, 3000);
       })
       .catch(err => {
         console.log(err);
