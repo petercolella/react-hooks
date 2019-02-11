8b5bd00f1e73fa52d0268b18e83adbc6c5ccd94c
petercolella <peter.joseph.colella@gmail.com>
2019-02-10 21:39:32 -0500

    Section 26, Lecture 471

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useReducer, useRef } from 'react';
+import React, { useState, useEffect, useReducer, useRef, useMemo } from 'react';
 import axios from 'axios';
 import List from './List';
 
@@ -122,7 +122,12 @@ const todo = props => {
       <button type="button" onClick={todoAddHandler}>
         Add
       </button>
-      <List items={todoList} onClick={todoRemoveHandler} />
+      {useMemo(
+        () => (
+          <List items={todoList} onClick={todoRemoveHandler} />
+        ),
+        [todoList]
+      )}
     </React.Fragment>
   );
 };
